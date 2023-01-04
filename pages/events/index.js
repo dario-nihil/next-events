0cefd56b1734302862e11b5dcff32585c5f447ba
dario-nihil <dario.nihil@gmail.com>
2023-01-04 19:17:09 +0100

    implemented events-search component and modified button component to render a link or a button

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,15 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventSearch from "./events-search";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
 
   return (
-    <div>
+    <>
+      <EventSearch />
       <EventList items={events} />
-    </div>
+    </>
   );
 };
 
