cb34ac7784a978d8301125635170b6c903201f6e
dario-nihil <dario.nihil@gmail.com>
2023-01-05 20:13:56 +0100

    implemented SSG for the home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
-
+const HomePage = ({ events }) => {
   return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
+    <>
+      <EventList items={events} />
+    </>
   );
 };
 
 export default HomePage;
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: { events: featuredEvents },
+  };
+}
