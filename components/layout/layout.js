eea57570dca797b11134e7027ce2843b47b0e5de
dario-nihil <dario.nihil@gmail.com>
2023-01-09 19:35:26 +0100

    added Notification component to Layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,24 @@
+import { useContext } from "react";
+
 import MainHeader from "./main-header";
+import NotificationContext from "../../store/notification-context";
+import Notification from "../../components/ui/notification";
 
 const Layout = (props) => {
+  const ctx = useContext(NotificationContext);
+  const activeNotification = ctx.notification;
+
   return (
     <>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   );
 };
