5114280017373b7918c4f1ad3f7a9eb0d91983ff
dario-nihil <dario.nihil@gmail.com>
2023-01-09 18:16:11 +0100

    installed mongodb, implemented get and post method for comments and post method for newsletter

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,13 +1,22 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   const eventId = req.query.eventId;
 
-  if (req.method === "GET") {
-    const dummyList = [
-      { id: "c1", name: "Max", text: "A first comment" },
-      { id: "c2", name: "Manuel", text: "A second comment" },
-    ];
+  const client = await MongoClient.connect(
+    "mongodb+srv://dario:BNFJu2qjdTQXsTv4@cluster0.ovnq1fr.mongodb.net/events?retryWrites=true&w=majority"
+  );
 
-    return res.status(200).json({ comments: dummyList });
+  if (req.method === "GET") {
+    const db = client.db();
+
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    return res.status(200).json({ comments: documents });
   }
 
   if (req.method === "POST") {
@@ -24,17 +33,24 @@ const handler = (req, res) => {
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
     };
 
-    console.log(newComment);
+    const db = client.db();
+
+    const result = await db.collection("comments").insertOne(newComment);
+
+    newComment.id = result.insertedId;
+
+    console.log(result);
     return res
       .status(201)
       .json({ message: "Added comment.", comment: newComment });
   }
+
+  client.close();
 };
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,10 +1,21 @@
-const handler = (req, res) => {
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const { email } = req.body;
 
     if (!email || !email.includes("@")) {
       return res.status(422).json({ message: "Invalid email address." });
     }
+
+    //"mongodb+srv://dario:BNFJu2qjdTQXsTv4@cluster0.w6fglbk.mongodb.net/newsletter?retryWrites=true&w=majority"
+    const client = await MongoClient.connect(
+      "mongodb+srv://dario:BNFJu2qjdTQXsTv4@cluster0.ovnq1fr.mongodb.net/events?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    await db.collection("newsletter").insertOne({ email: email });
+
+    client.close();
 
     return res.status(201).json({ message: "Signed up!" });
   }
